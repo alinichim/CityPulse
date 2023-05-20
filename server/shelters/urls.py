033fcc6701be3922f06bfab4e51d607689509f89
Alin Ichim <alinichim49@gmail.com>
2023-05-20 22:55:22 +0300

    Implemented user query.

--- server/shelters/urls.py
+++ server/shelters/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import return_shelters
+from .views import return_shelters, get_user
 
 urlpatterns = [
     path('shelters/', return_shelters),
+    path('user/', get_user),
 ]
--- server/shelters/views.py
+++ server/shelters/views.py
@@ -1,6 +1,7 @@
 from .models import Shelter
 from django.http import JsonResponse
 from users.views import tokens
+from django.views.decorators.csrf import csrf_exempt
 
 def return_shelters(request):
 
@@ -28,3 +29,13 @@ def return_shelters(request):
         return JsonResponse({"success": True, "safetyPlaces": data_q})
     
     return JsonResponse({"success": False, "error": "Invalid method"})
+
+def get_user(request):
+
+    if request.method == "GET":
+        # Check token.
+        if "Authorization" not in request.headers or request.headers["Authorization"] not in tokens:
+            return JsonResponse({"success": False, "error": "Invalid token"})
+        
+        user = tokens[request.headers["Authorization"]]
+        return JsonResponse({"success": True, "name": user.name, "email": user.email})
