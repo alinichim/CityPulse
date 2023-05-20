85f2c65e47d96239651174062a7bb6f49c86f298
Ionut <ionutclaudiu50@yahoo.com>
2023-05-20 16:59:15 +0300

    fixed settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2k7%j5hp+afabm=hk%17$az598_c&li=s+p6md!-mw+1n^%*&p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
