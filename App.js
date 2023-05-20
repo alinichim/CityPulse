7b944345e88a7c77068455442a30e53fbfa3ac47
Natanael Marian <natanaelmarian@Natanaels-MacBook-Pro.local>
2023-05-20 16:48:41 +0300

    Done maps on Client

--- App.js
+++ App.js
@@ -1,45 +1,64 @@
 import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Button, InputItem, Toast } from "@ant-design/react-native";
 
-function Card({ counts, setCounts, index }) {
-  function increaseElement(el, idx) {
-    if (idx == index) return el + 1;
-    else return el;
-  }
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  function increaseCount() {
-    const newState = counts.map(increaseElement);
-    setCounts(newState);
-    console.log(newState);
-  }
-  return (
-    <>
-      <Text>{counts[index]}</Text>
-      <Button title="Increase Count" onPress={increaseCount} />
-    </>
-  );
-}
-
-export default function App() {
-  const [counts, setCounts] = useState([10, 7, 99, 23]);
+  const handleLogin = () => {
+    if (username === "admin" && password === "password") {
+      Toast.success("Login successful");
+    } else {
+      Toast.fail("Invalid username or password");
+    }
+  };
 
   return (
     <View style={styles.container}>
-      {counts.map((element, index) => (
-        <Card key={index} counts={counts} setCounts={setCounts} index={index} />
-      ))}
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <InputItem
+        value={username}
+        onChange={(value) => setUsername(value)}
+        placeholder="Username"
+        style={styles.input}
+      />
+      <InputItem
+        value={password}
+        onChange={(value) => setPassword(value)}
+        placeholder="Password"
+        type="password"
+        style={styles.input}
+      />
+      <Button
+        type="primary"
+        onPress={handleLogin}
+        style={styles.button}
+        activeStyle={styles.buttonActive}
+      >
+        Login
+      </Button> */}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    padding: 16,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
+  },
+  input: {
+    marginBottom: 16,
+    borderRadius: 5,
+  },
+  button: {
+    borderRadius: 5,
+  },
+  buttonActive: {
+    backgroundColor: "#4CAAD7",
   },
 });
+
+export default LoginPage;
---
+++ components/MapComponent.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
+import ShelterMarker from "./ShelterMarker";
+const MapComponent = ({ shelters, filterShelters }) => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 44.4268,
+          longitude: 26.1025,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}
+        showsUserLocation // Enable showsUserLocation prop
+      >
+        {shelters.filter(filterShelters).map((shelter) => (
+          <ShelterMarker
+            key={shelter.name}
+            coordinate={{
+              latitude: shelter.latitude,
+              longitude: shelter.longitude,
+            }}
+            title={shelter.name}
+            description={shelter.address}
+            type={shelter.type}
+            capacity={shelter.capacity}
+            functionalities={shelter.functionalities}
+          />
+        ))}
+      </MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default MapComponent;
